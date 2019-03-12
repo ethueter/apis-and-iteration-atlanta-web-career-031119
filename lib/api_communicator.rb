638696d66dc082cbdfd0836ada8320e0bd54c3a1
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-12 16:01:20 -0400

    Completed pairing lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,9 +4,20 @@ require 'pry'
 
 def get_character_movies_from_api(character_name)
   #make the web request
+
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
 
+  char_data = response_hash["results"].find {|data| data["name"] == character_name}
+  film_url = char_data["films"]
+
+  new_array =[]
+  film_url.each do |film|
+    second_response = RestClient.get(film)
+    second_hash = JSON.parse(second_response)
+    new_array << second_hash
+  end
+  new_array
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -18,7 +29,13 @@ def get_character_movies_from_api(character_name)
   #  of movies by title. Have a play around with the puts with other info about a given film.
 end
 
+
+
 def print_movies(films)
+  films.each do |movie|
+    puts movie["title"]
+  end
+
   # some iteration magic and puts out the movies in a nice list
 end
 
@@ -27,6 +44,7 @@ def show_character_movies(character)
   print_movies(films)
 end
 
+show_character_movies("Luke Skywalker")
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
